2c5f971cd65d7931fcc75423971bb4f1f8f7476d
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-02 22:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let student ={
+    name: "vali"
+};
+
+Object.prototype.getKeys = function () {
+    let ans = [];
+    for (let key in this) {
+        if (this.hasOwnProperty(key)) {
+            ans.push(key);
+        }
+    }
+    return ans;
+};
+
+console.log(student.getKeys()); // Output: ['name']
